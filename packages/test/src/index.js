f1866df2c44bd4d00bed91803c908dbad74631fe
Diego Weinmann <diegoweinmann@gmail.com>
2022-03-01 17:39:20 -0300

    fix: imports and exports

--- packages/test/src/index.js
+++ packages/test/src/index.js
@@ -1,5 +1,8 @@
-import { add } from 'calc-webpack/dist/main.js';
+import { add } from 'calc-webpack';
+import { add as add2 } from 'calc-tsc';
 
 const result = add(10, 20);
+const result2 = add2(10, 20);
 
 console.log(result);
+console.log(result2);
